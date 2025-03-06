837a9ef0249792297a0fe5020477eb17b071cd10
Md Masum <masum@talentedcube.com>
2025-03-06 13:36:46 -0600

    stripe key is setting based on the env now.

--- src/config/stripe.php
+++ src/config/stripe.php
@@ -1,27 +1,21 @@
 <?php
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../utils/EnvUtil.php';
 
 class StripeConfig {
-    private static function loadEnv() {
-        $envFile = __DIR__ . '/../../.env';
-        if (file_exists($envFile)) {
-            $lines = file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-            foreach ($lines as $line) {
-                if (strpos($line, '=') !== false) {
-                    list($key, $value) = explode('=', $line, 2);
-                    $_ENV[trim($key)] = trim($value);
-                }
-            }
-        }
-    }
-
     public static function get($key) {
-        self::loadEnv();
         switch ($key) {
             case 'public_key':
-                return $_ENV['STRIPE_PUBLIC_KEY'] ?? null;
+                if (EnvUtil::isProduction()) {
+                    return EnvUtil::getEnv('STRIPE_PUBLIC_KEY_LIVE');
+                } else {
+                    return EnvUtil::getEnv('STRIPE_PUBLIC_KEY_TEST');
+                }
             case 'secret_key':
-                return $_ENV['STRIPE_SECRET_KEY'] ?? null;
+                if (EnvUtil::isProduction()) {
+                    return EnvUtil::getEnv('STRIPE_SECRET_KEY_LIVE');
+                } else {
+                    return EnvUtil::getEnv('STRIPE_SECRET_KEY_TEST');
+                }
             default:
                 return null;
         }
